1decae67812ae2216d80b2f7bd1313b2e0b73955
pczerski <blater@poczta.fm>
2017-07-07 12:25:48 +0200

    kolejny tab + cenniki

--- js/script.js
+++ js/script.js
@@ -2,20 +2,33 @@
 
 $(document).ready(function () {
     $(window).scroll();
+    //$('.parallax').parallax();
 });
 
 $(window).scroll(function () {
     changeMenu();
 });
 
+// SMOOTH SCROLL
+var root = $('html, body');
+$('a[href^="#"]').click(function(e) {
+    e.preventDefault();
+    var href = $(this).attr('href');
+    root.animate({
+        scrollTop: $(href).offset().top - 50
+    }, 500);
+});
+
 // MENU
 function changeMenu() {
     var navHeight = $('.navbar').height();
     var currentPosition = $(window).scrollTop();
-    
+
     if ($(window).scrollTop() > navHeight) {
         $('.navbar').addClass('menu-class');
-        $('.navbar').css({transition: 'all .5s'});
+        $('.navbar').css({
+            transition: 'all .5s'
+        });
         //$('#navigation a').css({color: 'aliceblue'});
     } else {
         $('.navbar').removeClass('menu-class');
@@ -28,28 +41,31 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  }
+    acc[i].onclick = function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    }
 }
 
 // GOOGLE MAP
 function myMap() {
-        var sunflower = {lat: 50.0977, lng: 19.9571};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 18,
-            mapTypeId: google.maps.MapTypeId.HYBRID,
-          center: sunflower
-        });
-        var marker = new google.maps.Marker({
-          position: sunflower,
-          map: map
-        });
-var Map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var sunflower = {
+        lat: 50.09769,
+        lng: 19.95715
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        center: sunflower
+    });
+    var marker = new google.maps.Marker({
+        position: sunflower,
+        map: map
+    });
+    var Map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
